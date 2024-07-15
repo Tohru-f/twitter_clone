72fde239cff2a7cf6475e14650f6102637cdc358
古澤透 <furusawatooru@furusawatoorunonotobukkukonpyuta.local>
2024-07-15 21:42:34 +0900

    change production.rb for confirmable email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,9 +67,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.delivery_method = :smtp
-  host = "#{ENV['HEROKU_APPNAME']}.herokuapp.com"
-  config.action_mailer.default_url_options = { host:, protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'https://sheltered-hollows-57037-d1f415cb80f0.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
@@ -82,7 +83,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
