e626550d36c80d5313570728671d95dff471f2f6
古澤透 <furusawatooru@furusawatoorunonotobukkukonpyuta.local>
2024-07-23 06:24:35 +0900

    reflect the outcome of rubocop

--- db/migrate/20240718111953_add_omniauth_to_users.rb
+++ db/migrate/20240718111953_add_omniauth_to_users.rb
@@ -2,7 +2,6 @@
 
 class AddOmniauthToUsers < ActiveRecord::Migration[7.0]
   def change
-
     change_table :users, bulk: true do |t|
       t.string :provider, null: false, default: ''
       t.string :uid
