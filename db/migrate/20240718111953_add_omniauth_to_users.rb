b8566818958211cf3dde21304bf48e9c10fde4b2
古澤透 <furusawatooru@furusawatoorunonotobukkukonpyuta.local>
2024-07-23 06:05:25 +0900

    amend migration file

--- db/migrate/20240718111953_add_omniauth_to_users.rb
+++ db/migrate/20240718111953_add_omniauth_to_users.rb
@@ -2,9 +2,10 @@
 
 class AddOmniauthToUsers < ActiveRecord::Migration[7.0]
   def change
+
     change_table :users, bulk: true do |t|
       t.string :provider, null: false, default: ''
-      t.string :users, :uid
+      t.string :uid
     end
 
     add_index :users, %i[uid provider], unique: true
