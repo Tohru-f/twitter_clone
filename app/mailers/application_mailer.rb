5f590715654441543d0a4333258887238cb48130
古澤透 <furusawatooru@furusawatoorunonotobukkukonpyuta.local>
2024-11-06 06:58:59 +0900

    modify application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['SENDER_ADDRESS']
+  default from: 'sampleact2024@gmail.com'
   layout 'mailer'
 end
